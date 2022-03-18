985ab0622500d291320fc80f160a5f25a2d399de
rossebell <Rosy24dr@outlook.com>
2022-03-18 17:08:47 -0400

    added the shopping cart class in the customer file

--- customer.py
+++ customer.py
@@ -3,4 +3,7 @@ from shopping_cart import Products_In_Cart
 class Customer:
     def __init__(self,name):
         self.customer_name = name
-        self.new_shopping_cart = Products_In_Cart
+
+
+def new_customer_product(self):
+    self.adding_new_product()
