a0fa3b81212acc41cf2b5e48adfae2db912090b9
rossebell <Rosy24dr@outlook.com>
2022-03-19 23:38:41 -0400

    end of day commit

--- customer.py
+++ customer.py
@@ -3,10 +3,11 @@ from shopping_cart import Products_In_Cart
 class Customer:
     def __init__(self,name):
         self.name = name
+        self.inventory = []
         self.new_product = Products_In_Cart()
 
 
-    def new_customer_product(self,item):
-        pass
+    # def new_customer_product(self,item):
+    #     self.inventory = Products_In_Cart()
         
 
